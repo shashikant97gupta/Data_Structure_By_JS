353bf470278de68178924257303cbc1f821ca6e9
skg97 <gshashikant@ramognee.in>
2023-12-14 22:46:32 +0530

    binary search implemented

--- binary_search.js
+++ binary_search.js
@@ -1,14 +1,22 @@
 function binarySearch(arr, low, high, key){
-   if(high >= low){
-    let mid = (high + low) // 2
-    if(arr[mid] == key) return mid;
+    //check if array has element
+    if(high>=low){
 
-    else if(arr[mid] > key) return binarySearch(arr, low, mid-1, key)
-    else return binarySearch(arr, mid+1, high, key)
-   }
-   else return -1     
+        //getting middle of the array
+        let mid = (high + low) // 2
+
+        //return if middle one is the key
+        if(arr[mid] == key) return mid
+
+        // if middle one is greater than key
+        else if(arr[mid] > key) return binarySearch(arr, low, mid-1, key)
+
+        else return binarySearch(arr, mid+1, high, key)
+    }
+    else return -1
 }
 
+//Driver code
 let arr = [2, 3, 4, 10, 40]
 let x = 10
  
